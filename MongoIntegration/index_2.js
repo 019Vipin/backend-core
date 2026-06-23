18be6000f6ab55d6e26cb1f16445b255a163ff23
019Vipin <utkarshraj8409@gmail.com>
2026-06-23 09:33:13 +0530

    backend-core

---
+++ MongoIntegration/index_2.js
@@ -0,0 +1,37 @@
+/**
+ * Logic to connect with the mongodb
+ */
+require("dotenv").config();
+const mongoose = require("mongoose");
+const Student= require("./models/students.model");
+const Address=require("./models/address.model");
+const studentsModel = require("./models/students.model");
+
+(async () => {
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/learn_mongoose");
+        console.log("MongoDB connection is successful !");
+        
+        /**
+         * Delete all the existing documents
+         */
+        await Promise.all([Address.deleteMany({}),studentsModel.deleteMany({})])
+
+        //insert the document/record in the students collection
+
+        const stud=await Student.create({
+            name:" abc",
+            contact:{phone:9999,guardian:"xyz"},
+            age:22,
+            email:"abc@gmail.com",
+            score:[10,99]
+        });
+        console.log(stud);
+
+    } catch (err) {
+        console.log("Mongo Error", err);
+    } finally {
+        await mongoose.disconnect();
+        console.log("Disconnected");
+    }
+})();
--- MongoIntegration/models/students.model.js
+++ MongoIntegration/models/students.model.js
@@ -3,10 +3,21 @@
  */
 const mongoose = require("mongoose");
 
+const contactSchema=new mongoose.Schema({
+    phone:{type:Number,match:/^[0-9]{10}$/}, //regular exp are meant for string datatype{match}
+    guardian:{type:String,trim:true}
+},{_id:false});
+
+
 const studentSchema = new mongoose.Schema({
-    name : String,
-    age : Number,
-    address:{type:mongoose.Schema.Types.ObjectId,ref:"Address"}
-})
+    name : {type:String,required:true,trim:true},
+    age : {type:Number,required:true,trim:true}, //natively defined constrainst
+    address:{type:mongoose.Schema.Types.ObjectId,ref:"Address"},
+    contact:{type:contactSchema,default:{}},
+    email:{type:String,required:true,unique:true,lowercase:true,trim:true},
+    score:{type:[Number],validate:v=>Array.isArray(v)&& v.every(n=> n>=0 && n<=100)} // user defined constraint using validate
+  },{timestamps:true});
+
+studentSchema.index({email:1}); 
 
 module.exports=mongoose.model("Student",studentSchema);
